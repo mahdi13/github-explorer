df429a1cdb22f68365cbd8afa08c4d04f098b91c
Mahdi Aali <mahdi13.1373@gmail.com>
2019-02-21 18:42:07 +0300

    Add some gradle dependencies

--- app/src/main/java/com/perfect/githubexplorer/data/db.kt
+++ app/src/main/java/com/perfect/githubexplorer/data/db.kt
@@ -1,2 +1,22 @@
 package com.perfect.githubexplorer.data
 
+//@Database(
+//    entities = [
+//        User::class,
+//        Repository::class
+//    ],
+//    version = 1
+//)
+//@TypeConverters(Converters::class)
+//abstract class StemeraldDatabase : RoomDatabase() {
+//    abstract val marketDao: MarketDao
+//    abstract val assetDao: AssetDao
+//    abstract val balanceDao: BalanceDao
+//    abstract val klineDao: KlineDao
+//    abstract val bookDao: BookDao
+//    abstract val dealDao: DealDao
+//    abstract val mineDao: MineDao
+//    abstract val userDao: UserDao
+//}
+//
+//lateinit var stemeraldDatabase: StemeraldDatabase
