c6a9ecb68933ec7f197f77b5bed5e9b02ea5fac6
Mahdi Aali <mahdi13.1373@gmail.com>
2019-02-21 18:37:45 +0300

    Add database and webservices

---
+++ app/src/main/java/com/perfect/githubexplorer/data/model.kt
@@ -0,0 +1,9 @@
+package com.perfect.githubexplorer.data
+
+data class User(
+    val username: String
+)
+
+data class Repository(
+    val name: String
+)
---
+++ app/src/main/java/com/perfect/githubexplorer/data/web.kt
@@ -0,0 +1,41 @@
+package com.perfect.githubexplorer.data
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+const val GITHUB_BASE_API_URL = "https://api.github.com"
+const val DEFAULT_PAGE_SIZE = 1
+
+@Suppress("DeferredIsResult")
+interface GithubApiInterface {
+
+    @GET("search/repositories")
+    fun serachRepositories(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") pageSize: Int = DEFAULT_PAGE_SIZE
+    ): Deferred<ArrayList<Repository>>
+
+    @GET("users/{username}")
+    fun userProfile(
+        @Path("username") username: String
+    ): Deferred<User>
+
+    @GET("users/{username}/repos")
+    fun userRepositories(
+        @Path("username") username: String
+    ): Deferred<ArrayList<User>>
+
+}
+
+var apiClient = Retrofit.Builder()
+    .baseUrl(GITHUB_BASE_API_URL)
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+    .create(GithubApiInterface::class.java)
