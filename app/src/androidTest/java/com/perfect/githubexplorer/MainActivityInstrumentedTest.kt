9681694d9c524ddcd68b355e46634ac160831539
Mahdi Aali <mahdi13.1373@gmail.com>
2019-02-27 14:25:56 +0300

    Add main activity instrumented test

---
+++ app/src/androidTest/java/com/perfect/githubexplorer/MainActivityInstrumentedTest.kt
@@ -0,0 +1,91 @@
+package com.perfect.githubexplorer
+
+import androidx.appcompat.widget.SearchView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.widget.EditText
+import android.widget.TextView
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.perfect.githubexplorer.ui.RepositoryViewHolder
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import com.google.android.material.chip.Chip
+import org.hamcrest.Matcher
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityInstrumentedTest {
+
+    @get:Rule
+    var activityRule: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
+
+    @Test
+    fun testRepositoryItemClick() {
+
+        onView(anyOf(instanceOf(EditText::class.java)))
+            .perform(clearText(), typeText("Abc"), pressImeActionButton())
+
+        Thread.sleep(3000)
+
+        onView(withId(R.id.list))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RepositoryViewHolder>(3, click()))
+
+
+        intended(hasComponent(RepositoryActivity::class.java.name))
+        intended(hasExtraWithKey("id"))
+
+    }
+
+    @Test
+    fun testRepositoryUserItemClick() {
+
+        onView(anyOf(instanceOf(EditText::class.java)))
+            .perform(clearText(), typeText("Abc"), pressImeActionButton())
+
+        Thread.sleep(3000)
+
+        onView(withId(R.id.list))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    3,
+                    object : ViewAction {
+                        override fun getConstraints(): Matcher<View>? {
+                            return null
+                        }
+
+                        override fun getDescription(): String {
+                            return "Click on specific button"
+                        }
+
+                        override fun perform(uiController: UiController, view: View) {
+                            val button = view.findViewById<Chip>(R.id.user)
+                            button.performClick()
+                        }
+                    })
+            )
+
+        intended(hasComponent(UserProfileActivity::class.java.name))
+        intended(hasExtraWithKey("username"))
+
+    }
+}
